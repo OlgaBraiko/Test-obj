ee90c36416f00905f2a705519b7e7b8e851441eb
Olga Braiko <olga.braiko456@mail.ru>
2023-05-15 11:11:51 -0700

    ed

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ const contents = document.querySelectorAll(".content");
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
     const contents = accordion.nextElementSibling;
-
-    contents.style.maxHeight
-      ? (contents.style.maxHeight = null)
-      : (contents.style.maxHeight = contents.scrollHeight + "px");
+    contents.style.maxHeight = contents.style.maxHeight
+      ? null
+      : contents.scrollHeight + "px";
   });
 });
+a = condition ? 1 : 0;
+
+condition ? (a = 1) : (a = 0);
