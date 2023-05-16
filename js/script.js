f839b3a31cf79d89a8f57ad5d65fb79ba76fbafe
Olga Braiko <olga.braiko456@mail.ru>
2023-05-16 14:23:40 -0700

    input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
- 
+const forms = document.querySelectorAll("form");
+const inputs = document.querySelectorAll("input");
+
+const messageUser = {
+  loading: "Загружаем...",
+  success: "Спасибо! С вами свяжется менеджер",
+  fail: "Ошибка...",
+};
+
+const postData = async (url, data) => {
+  const result = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "aplication/json",
+    },
+    body: data,
+  });
+  return await result.text();
+};
+
+const clearForms = () => {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+};
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const objDataForm = {};
+
+    formData.forEach((value, key) => (objDataForm[key] = value));
+    const json = JSON.stringify(objDataForm);
+
+    postData("https://simple-server-cumz.onrender.com/api/data", json).then(
+      () => {
+        clearForms();
+      }
+    );
+  });
+});
